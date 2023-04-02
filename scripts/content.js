af72157f3d6d60bca59ec331546833ff2223e7f8
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-04-01 23:11:13 -0700

    Add support for IEEE

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,17 @@
-const link = document.querySelector('.get-access');
+function updateIeeeLink(link) {
+    const offCampusUrl = window.location.toString().replace('https://ieeexplore.ieee.org/document/', 'https://ieeexplore-ieee-org.offcampus.lib.washington.edu/stamp/stamp.jsp?tp=&arnumber=')
+    link.href = offCampusUrl;
+    link.setAttribute('target', '_blank'); // open in new tab
+    link.setAttribute('title', 'Yay');
 
-if (link) {
-    const url = window.location.toString()
-    const offCampusUrl = url.replace('https://dl.acm.org/doi/abs/', 'https://dl-acm-org.offcampus.lib.washington.edu/doi/pdf/')
+    link.classList.add('pdf-btn-link')
+
+    const spanText = link.querySelector('span');
+    spanText.textContent = 'Open PDF';
+}
+
+function updateAcmLink(link) {
+    const offCampusUrl = window.location.toString().replace('https://dl.acm.org/doi/abs/', 'https://dl-acm-org.offcampus.lib.washington.edu/doi/pdf/')
 
     // Update access button to go to off campus link.
     link.href = offCampusUrl;
@@ -20,11 +29,40 @@ if (link) {
     // Update button style
     link.classList.add('btn');
     link.classList.add('red');
-    
+
     const i = link.querySelector('i');
     i.classList.remove('icon-get-access');
     i.classList.add('icon-pdf-file');
 
     const spanText = link.querySelector('span');
     spanText.textContent = 'Open PDF';
-  }
+}
+
+function main() {
+    const url = window.location.toString()
+
+    if (url.includes('ieeexplore.ieee.org')) {
+        // https://ieeexplore.ieee.org/document/8449656
+        // https://ieeexplore-ieee-org.offcampus.lib.washington.edu/document/8449656
+        // https://ieeexplore-ieee-org.offcampus.lib.washington.edu/stamp/stamp.jsp?tp=&arnumber=8449656
+
+        const link = document.querySelector('.doc-actions-link');
+        if (link) {
+            updateIeeeLink(link);
+        } else {
+            console.error('No link found');
+        }
+
+    } else if (url.includes('dl.acm.org')) {
+        const link = document.querySelector('.get-access');
+        if (link) {
+            updateAcmLink(link);
+        } else {
+            console.error('No link found');
+        }
+    } else {
+        console.error('Unsupported website');
+    }
+}
+
+main();
